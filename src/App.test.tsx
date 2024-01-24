f0f9fa6322bdbbfaf03c22feae4f12b6b7fb8dd0
gabrielinaciosouza <gabriel.sert9@gmail.com>
2024-01-24 23:20:21 +0100

    Modified App to improve route system and created unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders App component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByText("Gabriel's Blog")).toBeInTheDocument();
+  expect(screen.getByText("👋 Hi! I'm Gabriel")).toBeInTheDocument();
 });
